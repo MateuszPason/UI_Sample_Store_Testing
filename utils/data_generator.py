f539f8fd6cd08d3b71127fe0604cf46f47098933
Mateusz <mateusz@Mateuszs-MacBook-Air.local>
2026-08-02 18:53:54 +0200

    place order when logged in

--- utils/data_generator.py
+++ utils/data_generator.py
@@ -2,8 +2,10 @@ import uuid
 import json
 from config.paths import DATA_DIR
 
+
 def generate_random_uid() -> str:
     return uuid.uuid4().hex[:8]
+
 
 def generate_new_user_data() -> dict:
     with open(DATA_DIR / "users.json") as f:
@@ -13,24 +15,29 @@ def generate_new_user_data() -> dict:
     return {
         **static_user_data,
         "name": f"TestUser{uid}",
-        "email": generate_user_email()
+        "email": generate_user_email(),
     }
+
 
 def get_correct_login_data() -> dict:
     with open(DATA_DIR / "users.json") as f:
         return json.load(f)["correct_login_data"]
 
+
 def get_form_data() -> dict:
     with open(DATA_DIR / "form.json") as f:
         return json.load(f)["contact_us"]
+
 
 def get_search_data() -> dict:
     with open(DATA_DIR / "search.json") as f:
         return json.load(f)["scenarios"]
 
+
 def generate_user_email() -> str:
     uid = generate_random_uid()
     return f"testemail_{uid}@testemail.com"
+
 
 def get_credit_card_details() -> dict:
     with open(DATA_DIR / "card_payment.json") as f:
