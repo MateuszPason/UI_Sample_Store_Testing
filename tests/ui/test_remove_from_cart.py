a6577d2669f4a09bb0b3b1a7db00126f50e11453
Mateusz <mateusz@Mateuszs-MacBook-Air.local>
2026-08-06 22:07:17 +0200

    Remove multiple products from cart

--- tests/ui/test_remove_from_cart.py
+++ tests/ui/test_remove_from_cart.py
@@ -1,7 +1,17 @@
 from playwright.sync_api import expect
 
+
 class TestRemoveFromCart:
-    def test_remove_single_product_from_cart(self, home_page, page, config, cookie_modal, product_listing_page, basket_page, cart_modal):
+    def test_remove_single_product_from_cart(
+        self,
+        home_page,
+        page,
+        config,
+        cookie_modal,
+        product_listing_page,
+        basket_page,
+        cart_modal,
+    ):
         home_page.goto()
         expect(page).to_have_url(f"{config['base_url']}{home_page.PATH}")
 
@@ -15,3 +25,51 @@ class TestRemoveFromCart:
 
         expect(basket_page.cart_table).to_be_hidden()
         expect(basket_page.empty_cart_message).to_be_visible()
+
+    def test_remove_multiple_products_from_cart(
+        self,
+        page,
+        home_page,
+        config,
+        cookie_modal,
+        product_listing_page,
+        cart_modal,
+        header,
+        basket_page,
+    ):
+        home_page.goto()
+        expect(page).to_have_url(f"{config['base_url']}{home_page.PATH}")
+
+        cookie_modal.accept_default_value()
+
+        product_listing_page.add_to_cart_nth_product(1)
+        cart_modal.continue_shopping_after_add_to_cart()
+        product_listing_page.add_to_cart_nth_product(2)
+        cart_modal.continue_shopping_after_add_to_cart()
+        product_listing_page.add_to_cart_nth_product(3)
+        cart_modal.continue_shopping_after_add_to_cart()
+
+        header.go_to_cart()
+        expect(page).to_have_url(f"{config['base_url']}{basket_page.PATH}")
+
+        product_name_first = basket_page.get_product_name(1)
+        product_name_second = basket_page.get_product_name(2)
+        product_name_third = basket_page.get_product_name(3)
+
+        basket_page.remove_nth_product_row_from_basket(1)
+        expect(basket_page.get_product_row_by_row_number(1)).not_to_be_visible()
+        expect(
+            basket_page.cart_table.get_by_text(product_name_first, exact=True)
+        ).to_have_count(0)
+
+        basket_page.remove_nth_product_row_from_basket(2)
+        expect(basket_page.get_product_row_by_row_number(2)).not_to_be_visible()
+        expect(
+            basket_page.cart_table.get_by_text(product_name_second, exact=True)
+        ).to_have_count(0)
+
+        basket_page.remove_nth_product_row_from_basket(3)
+        expect(basket_page.get_product_row_by_row_number(3)).not_to_be_visible()
+        expect(
+            basket_page.cart_table.get_by_text(product_name_third, exact=True)
+        ).to_have_count(0)
